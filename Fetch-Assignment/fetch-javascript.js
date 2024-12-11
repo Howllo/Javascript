8464e857d9af0d008c339ed755b3e0a9190fcca9
Tony <allenr64y@ymail.com>
2024-12-11 15:53:32 -0600

    Fixed issue with repeating HTML id.

--- Fetch-Assignment/fetch-javascript.js
+++ Fetch-Assignment/fetch-javascript.js
@@ -15,9 +15,9 @@ function fetchInformation(){
                     <div id="div-${user.name}">
                         <h1 id='ul-${user.name}'>${user.name}</h1>
                         <ul id='ul-${user.name}'>
-                            <li id="li-company-name">Company Name: ${user.company.name}</li>
-                            <li id="li-website">Company Website: ${user.website}</li>
-                            <li id="li-address">Comapny Address:${user.address.street} ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}</li>
+                            <li id="li-${user.company.name}">Company Name: ${user.company.name}</li>
+                            <li id="li-${user.website}">Company Website: ${user.website}</li>
+                            <li id="li-${user.address.street}">Comapny Address:${user.address.street} ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}</li>
                         </ul>
                     </div>
                 `;
